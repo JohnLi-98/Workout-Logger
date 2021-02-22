d3dcf4acf8bec84a5fe49d4e95a2f97a3b73ab52
John Li <hud_johnli@live.co.uk>
2021-02-22 16:49:04 +0000

    added the user resolvers to login and register an account

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,7 +1,7 @@
+const usersResolver = require("./users");
+
 module.exports = {
-  Query: {
-    helloWorld() {
-      console.log("Hello World!");
-    },
+  Mutation: {
+    ...usersResolver.Mutation,
   },
 };
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,7 +1,28 @@
 const { gql } = require("apollo-server");
 
 module.exports = gql`
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    token: String!
+    createdAt: Float!
+  }
+  input LoginInput {
+    username: String!
+    password: String!
+  }
+  input RegisterInput {
+    username: String!
+    email: String!
+    password: String!
+    confirmPassword: String!
+  }
   type Query {
     helloWorld: String
+  }
+  type Mutation {
+    login(loginInput: LoginInput): User!
+    register(registerInput: RegisterInput): User!
   }
 `;
