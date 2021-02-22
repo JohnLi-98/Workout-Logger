858e4bdcfee5ccd9b5ad175cccd5816eaf1751ef
John Li <hud_johnli@live.co.uk>
2021-02-22 16:28:45 +0000

    created the models for user, set, exercise and workout

---
+++ server/graphql/models/Exercise.js
@@ -0,0 +1,19 @@
+const { model, Schema } = require("mongoose");
+
+const exerciseSchema = new Schema({
+  exerciseName: String,
+  sets: [
+    {
+      weight: Number,
+      reps: Number,
+      createdAt: Number,
+      notes: String,
+    },
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+});
+
+module.exports = model("Exercise", exerciseSchema);
