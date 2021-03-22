c49e315a1bb555e262a9c73f6e0b2c042a4a73c2
John Li <hud_johnli@live.co.uk>
2021-03-22 15:41:39 +0000

    refactored and commented all of graphql directory

--- server/utils/check-auth.js
+++ server/utils/check-auth.js
@@ -4,15 +4,14 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 
 module.exports = (context) => {
+  // Get auth string from header
   const authHeader = context.req.headers.authorization;
   if (authHeader) {
-    // If authHeader, retrieve the token from it
-    // Send header with a value Bearer <token_here>
-    // Token is in the second index of the split().
+    // Retrieve the token value from string (Bearer <token>)
     const token = authHeader.split("Bearer ")[1];
     if (token) {
-      // if there is a token, use jsonwebtoken to verify that the token matches with the secret key
       try {
+        // Verify token with jsonwebtoken and return user info if verified.
         const user = jwt.verify(token, SECRET_KEY);
         return user;
       } catch (err) {
